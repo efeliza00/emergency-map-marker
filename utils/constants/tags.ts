b9b13dff14529c205b055ad8d75dcf223919a48f
efeliza00 <evanfeliza22@gmail.com>
2025-11-12 13:21:11 +0800

    chore: add constant tags

---
+++ utils/constants/tags.ts
@@ -0,0 +1,12 @@
+export const tags = [
+  { id: "bus stop", label: "Near Bus Stop" },
+  { id: "main street", label: "On Main Street" },
+  { id: "city hall", label: "Across from City Hall" },
+  { id: "grocery store", label: "Near Grocery Store" },
+  { id: "park entrance", label: "At Park Entrance" },
+  { id: "hospital", label: "Near the Hospital" },
+  { id: "mall entrance", label: "Near Mall Entrance" },
+  { id: "police station", label: "Near Police Station" },
+  { id: "fire station", label: "Near Fire Station" },
+  { id: "public market", label: "At the Public Market" },
+];
