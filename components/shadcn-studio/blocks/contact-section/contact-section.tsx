3b5b1350d53b5ecf8e8ba1199695b750b74343a2
efeliza00 <evanfeliza22@gmail.com>
2025-11-16 20:32:09 +0800

    chore(contact): update mobile responsiveness

--- components/shadcn-studio/blocks/contact-section/contact-section.tsx
+++ components/shadcn-studio/blocks/contact-section/contact-section.tsx
@@ -3,11 +3,11 @@ import { Server } from "lucide-react";
 const ContactSection = () => {
   return (
     <div className=" max-w-full min-h-[calc(100dvh-4rem)] bg-accent">
-      <div className="max-w-7xl mx-auto py-20">
-        <div className=" bg-green-300/20  rounded-md drop-shadow-md p-4 mx-auto w-fit aspect-square">
-          <Server className="mx-auto size-20 text-green-500" />
+      <div className="max-w-7xl mx-auto px-4 md:px-0 py-20">
+        <div className=" bg-green-300/20  rounded-md drop-shadow-md p-4 my-4 md:my-0 mx-auto w-fit aspect-square">
+          <Server className="mx-auto size-10 md:size-20 text-green-500" />
         </div>
-        <h1 className="text-6xl font-semibold tracking-tight leading-44 text-center">
+        <h1 className="text-4xl md:text-6xl font-bold tracking-tight leading-tight md:leading-44 text-center">
           Keeping Our Services Online{" "}
           <span className="relative">
             24/7
@@ -28,13 +28,14 @@ const ContactSection = () => {
             </svg>
           </span>
         </h1>
-        <p className="text-xl">
+        <br />
+        <p className="text-lg md:text-xl">
           Our commitment is to provide reliable, uninterrupted access to our
           website&apos;s services around the clock. Maintaining the
           infrastructure and constant surveillance requires dedicated resources.
         </p>
         <br />
-        <p className="text-xl">
+        <p className="text-lg md:text-xl">
           We would be incredibly grateful for any contribution you can make to
           our operational fund. Your support ensures the continuous availability
           and performance of this site.
